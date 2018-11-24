509ee8e5d90432ebaa1825c8cb3951d239907822
Dario Giovannetti <dev@dariogiovannetti.net>
2018-11-25 01:51:30 +0800

    Change licence to MIT

--- README.md
+++ README.md
@@ -4,6 +4,4 @@ Create Flask REST APIs in peace.
 
 ## License
 
-Flask RIP is distributed under the terms of the
-[GNU General Public License v3.0](http://www.gnu.org/copyleft/gpl.html)
-(see LICENSE).
+Flask RIP is distributed under the terms of the MIT licence (see LICENSE).
--- setup.py
+++ setup.py
@@ -9,20 +9,20 @@ setup(
     url='https://github.com/kynikos/lib.py.flask-rip',
     author='Dario Giovannetti',
     author_email='dev@dariogiovannetti.net',
-    license='GPLv3+',
+    license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Flask',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',  # noqa
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',  # noqa
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
     ],
     keywords='flask rest restful api marshal marshmallow',
     py_modules=["flask_rip"],
